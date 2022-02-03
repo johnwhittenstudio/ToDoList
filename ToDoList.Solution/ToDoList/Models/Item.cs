bc50f881c2ca1d3e574505b76432f8861623eaf0
Phil Curran <pecurran@hotmail.com>
2022-02-02 19:55:42 -0800

    initial commit; two tests passing.

---
+++ ToDoList.Solution/ToDoList/Models/Item.cs
@@ -0,0 +1,13 @@
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; }
+
+    public Item(string description)
+    {
+      Description = description;
+    }
+  }
+}
